38fb4a31ed266cc7de5cf1a5cccabd2e027f963f
David Laprade <david.laprade@gmail.com>
2024-11-21 10:49:59 -0500

    Use checkpoints also

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.26;
+pragma solidity ^0.8.20;
 
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import {Checkpoints} from "@openzeppelin/contracts/utils/structs/Checkpoints.sol";
 
 contract Counter {
+  using SafeCast for uint256;
+  using Checkpoints for Checkpoints.Trace208;
   uint256 public number;
 
   function setNumber(uint256 newNumber) public {
