ff1a08e7c204440a29d29dac306c6743950fe1be
David Laprade <david.laprade@gmail.com>
2024-11-21 10:46:41 -0500

    Use safecast

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.26;
 
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
 contract Counter {
   uint256 public number;
 
   function setNumber(uint256 newNumber) public {
+    SafeCast.toUint256(42);
     number = newNumber;
   }
 
