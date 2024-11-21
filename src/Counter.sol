4b210346a981ab715385c00e0f71594f0d99e28a
David Laprade <david.laprade@gmail.com>
2024-11-21 11:51:45 -0500

    Remove safecast

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
-
 contract Counter {
-  using SafeCast for uint256;
   uint256 public number;
 
   function setNumber(uint256 newNumber) public {
-    SafeCast.toUint256(42);
     number = newNumber;
   }
 
